76c85e777350758bf32f6484fa6cd4bfccf11f97
AdamJohnSwan <adam@labod.co>
2017-12-10 16:49:29 -0500

    Removed need to load jquery and the loader icon

--- popup.js
+++ popup.js
@@ -3,14 +3,7 @@
 /*
 * This is the javascript file which is used to implement the functionalities of extension
 */
-// Pre loader jquery code
-jQuery(document).ready(function($) {  
 
-$(window).load(function(){
-  $('#preloader').fadeOut('slow',function(){$(this).remove();});
-});
-
-});
 
 var source;
 var xmlhttp;
@@ -65,37 +58,44 @@ getURL("address", function(x){
             var xmlhttp = new XMLHttpRequest();
             xmlhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
+                	  preloader = document.getElementById("preloader");
+					  preloader.classList.add('fade');
+                	  
                       myObj = JSON.parse(this.responseText);
                       
                       var div = document.getElementById('card_container');
-                      div.innerHTML = "";
+                 
+                      
+					  preloader.addEventListener('webkitAnimationEnd',function( event ) {
+					  	  preloader.style.display = 'none';
+					  	  div.innerHTML = "";
+					  	  
+		                  if(typeof myObj.items == 'undefined'){
+		                    div.innerHTML = "<center><h1>No results found.</h1><center>";
+		                  }
 
-                      if(typeof myObj.items == 'undefined'){
-                        div.innerHTML = "<center><h1>No results found.</h1><center>";
-                      }
+		                  for (var i = 0; i < myObj.items.length; i++) {
+		                    if(myObj.items[i].link == URLC)
+		                      continue;
+		                    data = "<a target='_blank' title='"+ myObj.items[i].displayLink +"' href=" + myObj.items[i].link+"><div id='card'>";
 
-                      for (var i = 0; i < myObj.items.length; i++) {
-                        if(myObj.items[i].link == URLC)
-                          continue;
-                        data = "<a target='_blank' title='"+ myObj.items[i].displayLink +"' href=" + myObj.items[i].link+"><div id='card'>";
+		                    if( (typeof myObj.items[i].pagemap != 'undefined')){
+		                      if(typeof myObj.items[i].pagemap.cse_image != 'undefined')
+		                          data+="<img src='"+ myObj.items[i].pagemap.cse_image[0].src +"'/>";
+		                      else
+		                          data+="<img src='icon.png'/>";
+		                    }
+		                    else{
+		                      data+="<img src='icon.png'/>";
+		                    }
 
-                        if( (typeof myObj.items[i].pagemap != 'undefined')){
-                          if(typeof myObj.items[i].pagemap.cse_image != 'undefined')
-                              data+="<img src='"+ myObj.items[i].pagemap.cse_image[0].src +"'/>";
-                          else
-                              data+="<img src='icon.png'/>";
+
+		                    data1 = "<h2 id='header'>"+ myObj.items[i].title+"</h2>";
+		                    data2 = "<p id='snippet'>"+myObj.items[i].htmlSnippet+"</p></div></a>";
+
+		                    div.innerHTML = div.innerHTML + data + data1 + data2;
                         }
-                        else{
-                          data+="<img src='icon.png'/>";
-                        }
-
-
-                        data1 = "<h2 id='header'>"+ myObj.items[i].title+"</h2>";
-                        data2 = "<p id='snippet'>"+myObj.items[i].htmlSnippet+"</p></div></a>";
-
-                        div.innerHTML = div.innerHTML + data + data1 + data2;
-                        
-                    }
+                    }, false);
                 }
             };
 
